7717c988fdae37e2747a1f4091d3f7861aaa9483
Alexander Bykov <alexbykov95@gmail.com>
2026-02-24 12:23:12 +0300

    Добавление DB в config.py

--- bot/config/config.py
+++ bot/config/config.py
@@ -7,19 +7,28 @@ class TgBot:
     token: str
     admin_ids: list[int]
 
+
+@dataclass
+class DatabaseConfig:
+    database_url: str
+
+
 @dataclass
 class Config:
     bot: TgBot
+    db: DatabaseConfig
 
 
 def load_config(path: str | None = None) -> Config:
-
     env: Env = Env()
-    env.read_env()
+    env.read_env(path)
 
     return Config(
-        bot = TgBot(
-            token = env("BOT_TOKEN"),
-            admin_ids = list(map(int, env.list("ADMIN_IDS")))
+        bot=TgBot(
+            token=env("BOT_TOKEN"),
+            admin_ids=list(map(int, env.list("ADMIN_IDS")))
+        ),
+        db=DatabaseConfig(
+            database_url=env("DATABASE_URL")
         )
     )
