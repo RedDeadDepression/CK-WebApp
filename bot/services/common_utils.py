4281d7fb8378f09df68ef8d4efeead836ecced8a
Alexander Bykov <alexbykov95@gmail.com>
2026-03-05 11:00:14 +0300

    Дополнение функции show_main_menu

--- bot/services/common_utils.py
+++ bot/services/common_utils.py
@@ -14,15 +14,39 @@ async def show_main_menu(
         text: str = "🏠 MAIN MENU",
         db
 ) -> None:
+
     user_id = callback.from_user.id
     survey_status = await db.is_survey_completed(user_id)
 
     if clear_state:
         await state.clear()
 
-    await callback.message.edit_text(
-        text,
-        reply_markup = get_main_menu_keyboard(survey_status)
-    )
+    message = callback.message
+
+    # если текущее сообщение фото или caption
+    if message.photo or message.caption:
+        await message.delete()
+
+        await callback.bot.send_message(
+            chat_id=message.chat.id,
+            text=text,
+            reply_markup=get_main_menu_keyboard(survey_status)
+        )
+
+    # если обычное текстовое сообщение
+    else:
+        try:
+            await message.edit_text(
+                text,
+                reply_markup=get_main_menu_keyboard(survey_status)
+            )
+        except Exception:
+            # fallback если edit невозможен
+            await message.delete()
+            await callback.bot.send_message(
+                chat_id=message.chat.id,
+                text=text,
+                reply_markup=get_main_menu_keyboard(survey_status)
+            )
 
     await callback.answer()
