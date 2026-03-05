9374be6b489558b31daad0e001319fefb87d91fa
Alexander Bykov <alexbykov95@gmail.com>
2026-03-05 10:10:57 +0300

    Добавлена универнсальная функция отправки шага send_step

--- bot/handlers/survey.py
+++ bot/handlers/survey.py
@@ -319,6 +319,36 @@ async def process_finish(callback: CallbackQuery, state: FSMContext, db: Databas
 
 
 # ================= ONBOARDING =================
+async def send_step(callback: CallbackQuery, text: str, button: str, image_name: str | None):
+
+    if image_name:
+        photo = FSInputFile(f"images/{image_name}")
+
+        await callback.message.delete()
+
+        await callback.message.answer_photo(
+            photo=photo,
+            caption=text,
+            reply_markup=onboarding_keyboard(button),
+            parse_mode="HTML"
+        )
+
+    else:
+        if callback.message.photo:
+            await callback.message.delete()
+
+            await callback.message.answer(
+                text,
+                reply_markup=onboarding_keyboard(button),
+                parse_mode="HTML"
+            )
+        else:
+            await callback.message.edit_text(
+                text,
+                reply_markup=onboarding_keyboard(button),
+                parse_mode="HTML"
+            )
+
 @survey_router.callback_query(F.data == "keep_it_button_click", FSMSurvey.calculating)
 async def start_onboarding(callback: CallbackQuery, state: FSMContext):
     await state.set_state(FSMSurvey.onboarding)
