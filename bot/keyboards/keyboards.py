ef7e5be36051b651d52424d4fc8137aec322bb47
Alexander Bykov <alexbykov95@gmail.com>
2026-04-28 18:43:30 +0300

    Изменение web_app url

--- bot/keyboards/keyboards.py
+++ bot/keyboards/keyboards.py
@@ -21,7 +21,7 @@ def get_main_menu_keyboard(is_survey_completed: bool):
         InlineKeyboardButton(
             text = "🚭 SOS / I want to smoke",
             web_app = WebAppInfo(
-                url = "https://ck-webapp-production.up.railway.app" # ЗАМЕНИТЬ
+                url = "ck-webapp-production-13ce.up.railway.app" # ЗАМЕНИТЬ
             )
         )
     )
